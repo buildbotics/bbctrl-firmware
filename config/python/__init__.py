8dc213baf1f7afad60664779c4a1a4c54f10f482
Joseph Coffland <joseph@cauldrondevelopment.com>
2014-04-25 15:23:56 -0700

    Revert python config changes

--- config/python/__init__.py
+++ config/python/__init__.py
@@ -3,15 +3,6 @@ import inspect
 import traceback
 
 
-def check_config(conf):
-    env = conf.env
-    conf.CBRequireHeader('Python.h')
-    conf.CBRequireFunc('Py_Initialize')
-    env.CBDefine('HAVE_PYTHON')
-    env.Prepend(LIBS = ['util', 'm', 'dl', 'z'])
-    return True
-
-
 def try_config(conf, command):
     env = conf.env
 
@@ -22,21 +13,20 @@ def try_config(conf, command):
     except OSError:
         return False
 
-    try:
-        return check_config(conf)
-    except:
-        return False
+    if conf.CheckHeader('Python.h') and conf.CheckFunc('Py_Initialize'):
+        env.ParseConfig(command)
+        env.CBDefine('HAVE_PYTHON');
+        env.Prepend(LIBS = ['util', 'm', 'dl', 'z'])
 
+        return True
+
+    return False
+    
 
 def configure(conf):
     env = conf.env
 
     if not env.get('python', 0): return False
-
-    home = conf.CBCheckHome('python', inc_suffix = '/Include /include')
-    if home:
-        env.AppendUnique(CPPPATH = [home])
-        return check_config(conf)
 
     python_version = env.get('python_version', '')
 
