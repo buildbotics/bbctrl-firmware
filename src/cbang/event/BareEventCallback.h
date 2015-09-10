26e4fac3f6896472511c21b139d9a29e57c3f4aa
Joseph Coffland <joseph@cauldrondevelopment.com>
2015-09-09 23:09:48 -0700

    Added bare event callbacks

---
+++ src/cbang/event/BareEventCallback.h
@@ -0,0 +1,55 @@
+/******************************************************************************\
+
+          This file is part of the C! library.  A.K.A the cbang library.
+
+              Copyright (c) 2003-2015, Cauldron Development LLC
+                 Copyright (c) 2003-2015, Stanford University
+                             All rights reserved.
+
+        The C! library is free software: you can redistribute it and/or
+        modify it under the terms of the GNU Lesser General Public License
+        as published by the Free Software Foundation, either version 2.1 of
+        the License, or (at your option) any later version.
+
+        The C! library is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+        Lesser General Public License for more details.
+
+        You should have received a copy of the GNU Lesser General Public
+        License along with the C! library.  If not, see
+        <http://www.gnu.org/licenses/>.
+
+        In addition, BSD licensing may be granted on a case by case basis
+        by written permission from at least one of the copyright holders.
+        You may request written permission by emailing the authors.
+
+                For information regarding this software email:
+                               Joseph Coffland
+                        joseph@cauldrondevelopment.com
+
+\******************************************************************************/
+
+#ifndef CB_EVENT_BARE_EVENT_CALLBACK_H
+#define CB_EVENT_BARE_EVENT_CALLBACK_H
+
+#include "EventCallback.h"
+
+namespace cb {
+  namespace Event {
+    class BareEventCallback : public EventCallback {
+    public:
+      virtual void operator()() = 0;
+
+      // From EventCallback
+      void operator()(Event &event, int fd, unsigned flags) {(*this)();}
+    };
+
+    CBANG_FUNCTOR(BareEventFunctor, BareEventCallback, void, operator());
+    CBANG_MEMBER_FUNCTOR(BareEventMemberFunctor, BareEventCallback, void,
+                         operator());
+  }
+}
+
+#endif // CB_EVENT_BARE_EVENT_CALLBACK_H
+
