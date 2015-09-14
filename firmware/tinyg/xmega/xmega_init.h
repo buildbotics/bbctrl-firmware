995eb4e186650e847394047a4af78b9018fd7d41
Alden Hart <alden08@harts.org>
2015-09-14 14:36:05 -0400

    testing arcs

---
+++ firmware/tinyg/xmega/xmega_init.h
@@ -0,0 +1,30 @@
+/*
+ * xmega_init.h - general init and support functions for xmega family
+ * Part of TinyG project
+ *
+ * Copyright (c) 2010 - 2013 Alden S. Hart Jr.
+ *
+ * This file ("the software") is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2 as published by the
+ * Free Software Foundation. You should have received a copy of the GNU General Public
+ * License, version 2 along with the software.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THE SOFTWARE IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL, BUT WITHOUT ANY
+ * WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+ * SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef xmega_support_h
+#define xmega_support_h
+
+/*
+ * Global Scope Functions
+ */
+
+void xmega_init(void);
+void CCPWrite( volatile uint8_t * address, uint8_t value );
+
+#endif
