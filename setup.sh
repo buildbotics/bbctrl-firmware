460c12f1afb5b8032e125ed330b473df1eb6e5a7
Joseph Coffland <joseph@cauldrondevelopment.com>
2015-10-30 14:21:40 -0700

    Setup server

--- setup.sh
+++ setup.sh
@@ -7,14 +7,14 @@ apt-get dist-upgrade -y
 # Install the auto discovery daemon
 apt-get install -y avahi-daemon
 
+# Clean
+apt-get autoclean
+
 # Change the hostname
 sed -i 's/raspberrypi/bbctrl/' /etc/hosts /etc/hostname
 
 # Create bb user
-useradd -m -p buildbotics -s /bin/bash user
-
-# Remove pi user
-cd /
-userdel -rf pi
+useradd -m -p $(openssl passwd -1 buildbotics) -s /bin/bash bbmc
+echo "bbmc ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 reboot
