d10fd48229fc61de0b272d0f63da3528a133d324
Joseph Coffland <joseph@cauldrondevelopment.com>
2016-05-30 02:02:28 -0700

    Shouldn't need librt for OSX cross build

--- config/cbang/__init__.py
+++ config/cbang/__init__.py
@@ -50,7 +50,7 @@ def configure_deps(conf, local = True, with_openssl = True):
         ConfigBoost(conf, True)
 
     # clock_gettime() needed by boost iterprocess
-    if env['PLATFORM'] == 'posix' and not int(env.get('cross_osx', 0)) \
+    if env['PLATFORM'] == 'posix' and int(env.get('cross_osx', 0)) == 0 \
             and not conf.CBCheckFunc('clock_gettime'):
         conf.CBRequireLib('rt')
         conf.CBRequireFunc('clock_gettime')
