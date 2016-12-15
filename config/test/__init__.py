c7f7337b8f86786cd74f46cee9cff7f6d8eb8ec3
Joseph Coffland <joseph@cauldrondevelopment.com>
2016-12-15 14:10:34 -0800

    Use color output for tests outside of dockbot

--- config/test/__init__.py
+++ config/test/__init__.py
@@ -1,19 +1,24 @@
 from SCons.Script import *
-import subprocess
-import sys
-import shlex
 
 
 def run_tests(env):
+    import shlex
+    import subprocess
+    import sys
+
     subprocess.call(shlex.split(env.get('TEST_COMMAND')))
     sys.exit(0)
 
 
 def generate(env):
-    env.CBAddVariables(('TEST_COMMAND', 'Command to run for `test` target',
-                        'tests/testHarness -C tests --no-color --diff-failed '
-                        '--view-failed --view-unfiltered --save-failed '
-                        '--build'))
+    import os
+
+    cmd = 'tests/testHarness -C tests --diff-failed --view-failed ' \
+        '--view-unfiltered --save-failed --build'
+
+    if 'DOCKBOT_MASTER_PORT' in os.environ: cmd += ' --no-color'
+
+    env.CBAddVariables(('TEST_COMMAND', '`test` target command line', cmd))
 
     if 'test' in COMMAND_LINE_TARGETS: env.CBAddConfigFinishCB(run_tests)
 
