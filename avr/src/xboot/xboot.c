3e070526afcc76e314c7d3111b952ef19acdf89c
Joseph Coffland <joseph@cauldrondevelopment.com>
2017-01-14 00:54:20 -0800

    Cleaned up bootloader code

--- avr/src/xboot/xboot.c
+++ avr/src/xboot/xboot.c
@@ -1,102 +1,246 @@
-/************************************************************************/
-/* XBoot Extensible AVR Bootloader                                      */
-/*                                                                      */
-/* xboot.c                                                              */
-/*                                                                      */
-/* Alex Forencich <alex@alexforencich.com>                              */
-/*                                                                      */
-/* Copyright (c) 2010 Alex Forencich                                    */
-/*                                                                      */
-/* Permission is hereby granted, free of charge, to any person          */
-/* obtaining a copy of this software and associated documentation       */
-/* files(the "Software"), to deal in the Software without restriction,  */
-/* including without limitation the rights to use, copy, modify, merge, */
-/* publish, distribute, sublicense, and/or sell copies of the Software, */
-/* and to permit persons to whom the Software is furnished to do so,    */
-/* subject to the following conditions:                                 */
-/*                                                                      */
-/* The above copyright notice and this permission notice shall be       */
-/* included in all copies or substantial portions of the Software.      */
-/*                                                                      */
-/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,      */
-/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF   */
-/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND                */
-/* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS  */
-/* BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN   */
-/* ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN    */
-/* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE     */
-/* SOFTWARE.                                                            */
-/*                                                                      */
-/************************************************************************/
+/******************************************************************************\
+
+                This file is part of the Buildbotics firmware.
+
+                  Copyright (c) 2015 - 2016 Buildbotics LLC
+          Copyright (c) 2010 Alex Forencich <alex@alexforencich.com>
+                            All rights reserved.
+
+     This file ("the software") is free software: you can redistribute it
+     and/or modify it under the terms of the GNU General Public License,
+      version 2 as published by the Free Software Foundation. You should
+      have received a copy of the GNU General Public License, version 2
+     along with the software. If not, see <http://www.gnu.org/licenses/>.
+
+     The software is distributed in the hope that it will be useful, but
+          WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+               Lesser General Public License for more details.
+
+       You should have received a copy of the GNU Lesser General Public
+                License along with the software.  If not, see
+                       <http://www.gnu.org/licenses/>.
+
+                For information regarding this software email:
+                  "Joseph Coffland" <joseph@buildbotics.com>
+
+\******************************************************************************/
 
 #include "xboot.h"
+#include "sp_driver.h"
 
 #include <util/delay.h>
+#include <util/crc16.h>
+
+#include <avr/eeprom.h>
+#include <avr/io.h>
 
 #include <stdbool.h>
 
 
 uint8_t buffer[SPM_PAGESIZE];
+uint16_t block_crc = 0;
 
 
-void NVM_Wait() {
-  while (NVM_STATUS & NVM_NVMBUSY_bp)
-    // reset watchdog while waiting for erase completion
-    WDT_Reset();
+void clock_init() {
+  // External 16Mhz Xtal w/ 2x PLL = 32 Mhz
+  // 12-16 MHz crystal; 0.4-16 MHz XTAL w/ 16K CLK startup
+  OSC.XOSCCTRL = OSC_FRQRANGE_12TO16_gc | OSC_XOSCSEL_XTAL_16KCLK_gc;
+  OSC.CTRL = OSC_XOSCEN_bm;                // enable external crystal oscillator
+  while (!(OSC.STATUS & OSC_XOSCRDY_bm));  // wait for oscillator ready
+
+  OSC.PLLCTRL = OSC_PLLSRC_XOSC_gc | 2;    // PLL source, 2x (32 MHz sys clock)
+  OSC.CTRL = OSC_PLLEN_bm | OSC_XOSCEN_bm; // Enable PLL & External Oscillator
+  while (!(OSC.STATUS & OSC_PLLRDY_bm));   // wait for PLL ready
+
+  CCP = CCP_IOREG_gc;
+  CLK.CTRL = CLK_SCLKSEL_PLL_gc;           // switch to PLL clock
+  OSC.CTRL &= ~OSC_RC2MEN_bm;              // disable internal 2 MHz clock
+}
+
+
+bool uart_has_char() {return UART_DEVICE.STATUS & USART_RXCIF_bm;}
+uint8_t uart_recv_char() {return UART_DEVICE.DATA;}
+
+
+void uart_send_char_blocking(uint8_t c) {
+  UART_DEVICE.DATA = c;
+  while (!(UART_DEVICE.STATUS & USART_TXCIF_bm)) continue;
+  UART_DEVICE.STATUS |= USART_TXCIF_bm;
+}
+
+
+void uart_init() {
+  UART_PORT.DIRSET = 1 << UART_TX_PIN;
+  UART_DEVICE.BAUDCTRLA = UART_BSEL_VALUE & USART_BSEL_gm;
+  UART_DEVICE.BAUDCTRLB =
+    ((UART_BSCALE_VALUE << USART_BSCALE_gp) & USART_BSCALE_gm) |
+    ((UART_BSEL_VALUE >> 8) & ~USART_BSCALE_gm);
+  UART_DEVICE.CTRLB = USART_RXEN_bm | USART_TXEN_bm;
+
+  PORTC.OUTCLR = 1 << 4; // CTS Lo (enable)
+  PORTC.DIRSET = 1 << 4; // CTS Output
+}
+
+
+void uart_deinit() {
+  UART_DEVICE.CTRLB = 0;
+  UART_DEVICE.BAUDCTRLA = 0;
+  UART_DEVICE.BAUDCTRLB = 0;
+  UART_PORT.DIRCLR = 1 << UART_TX_PIN;
+}
+
+
+void watchdog_init() {
+  uint8_t temp = WDT_ENABLE_bm | WDT_CEN_bm | WDT_PER_1KCLK_gc;
+  CCP = CCP_IOREG_gc;
+  WDT.CTRL = temp;
+  while (WDT.STATUS & WDT_SYNCBUSY_bm) continue;
+}
+
+
+void watchdog_reset() {asm("wdr");}
+
+
+void watchdog_disable() {
+  uint8_t temp = (WDT.CTRL & ~WDT_ENABLE_bm) | WDT_CEN_bm;
+  CCP = CCP_IOREG_gc;
+  WDT.CTRL = temp;
+}
+
+
+void nvm_wait() {while (NVM_STATUS & NVM_NVMBUSY_bp) watchdog_reset();}
+
+
+void nvm_exec() {
+  void *z = (void *)&NVM_CTRLA;
+
+  __asm__ volatile
+    ("out %[ccp], %[ioreg]\n"
+     "st z, %[cmdex]" ::
+     [ccp] "I" (_SFR_IO_ADDR(CCP)),
+     [ioreg] "d" (CCP_IOREG_gc),
+     [cmdex] "r" (NVM_CMDEX_bm),
+     [z] "z" (z));
+}
+
+
+uint8_t get_char() {
+  while (!uart_has_char()) continue;
+  return uart_recv_char();
+}
+
+
+void send_char(uint8_t c) {uart_send_char_blocking(c);}
+
+
+uint16_t get_word() {
+  uint8_t hi = get_char();
+  uint8_t lo = get_char();
+  return ((uint16_t)hi << 8) | lo;
+}
+
+
+uint8_t BlockLoad(unsigned size, uint8_t mem, uint32_t *address) {
+  watchdog_reset();
+
+  // fill up buffer
+  block_crc = 0xffff;
+  for (int i = 0; i < SPM_PAGESIZE; i++)
+    if (i < size) {
+      buffer[i] = get_char();
+      block_crc = _crc16_update(block_crc, buffer[i]);
+
+    } else buffer[i] = 0xff;
+
+  switch (mem) {
+  case MEM_EEPROM:
+    eeprom_write_block(buffer, (uint8_t *)(uint16_t)*address, size);
+    *address += size;
+    break;
+
+  case MEM_FLASH:
+    // NOTE: Flash programming, address is given in words.
+    SP_LoadFlashPage(buffer);
+    SP_EraseWriteApplicationPage(*address << 1);
+    *address += size >> 1;
+    nvm_wait();
+    break;
+
+  case MEM_USERSIG:
+    SP_LoadFlashPage(buffer);
+    SP_EraseUserSignatureRow();
+    nvm_wait();
+    SP_WriteUserSignatureRow();
+    nvm_wait();
+    break;
+
+  default: return REPLY_ERROR;
+  }
+
+  return REPLY_ACK;
+}
+
+
+
+void BlockRead(unsigned size, uint8_t mem, uint32_t *address) {
+  switch (mem) {
+  case MEM_EEPROM:
+    eeprom_read_block(buffer, (uint8_t *)(uint16_t)*address, size);
+    *address += size;
+
+    // send bytes
+    for (int i = 0; i < size; i++)
+      send_char(buffer[i]);
+    break;
+
+  case MEM_FLASH: case MEM_USERSIG: case MEM_PRODSIG: {
+    *address <<= 1; // Convert address to bytes temporarily
+
+    do {
+      switch (mem) {
+      case MEM_FLASH: send_char(SP_ReadByte(*address)); break;
+      case MEM_USERSIG: send_char(SP_ReadUserSignatureByte(*address)); break;
+      case MEM_PRODSIG: send_char(SP_ReadCalibrationByte(*address)); break;
+      }
+
+      nvm_wait();
+
+      (*address)++;    // Select next word in memory.
+      size--;          // Subtract two bytes from number of bytes to read
+    } while (size);    // Repeat until all block has been read
+
+    *address >>= 1;    // Convert address back to Flash words again.
+    break;
+  }
+
+  default: break;
+  }
 }
 
 
 int main() {
-  // Initialization section
-  // Entry point and communication methods are initialized here
-  // --------------------------------------------------
-#ifdef USE_32MHZ_RC
-#if (F_CPU != 32000000L)
-#error F_CPU must match oscillator setting!
-#endif // F_CPU
-  OSC.CTRL |= OSC_RC32MEN_bm; // turn on 32 MHz oscillator
-  while (!(OSC.STATUS & OSC_RC32MRDY_bm)) continue; // wait for it to start
-  CCP = CCP_IOREG_gc;
-  CLK.CTRL = CLK_SCLKSEL_RC32M_gc;
-#ifdef USE_DFLL
-  OSC.CTRL |= OSC_RC32KEN_bm;
-  while(!(OSC.STATUS & OSC_RC32KRDY_bm));
-  DFLLRC32M.CTRL = DFLL_ENABLE_bm;
-#endif // USE_DFLL
-#else // USE_32MHZ_RC
-#if (F_CPU != 2000000L)
-#error F_CPU must match oscillator setting!
-#endif // F_CPU
-#ifdef USE_DFLL
-  OSC.CTRL |= OSC_RC32KEN_bm;
-  while(!(OSC.STATUS & OSC_RC32KRDY_bm));
-  DFLLRC2M.CTRL = DFLL_ENABLE_bm;
-#endif // USE_DFLL
-#endif // USE_32MHZ_RC
-
-  // Initialize UART
+  // Init
+  clock_init();
   uart_init();
+  watchdog_init();
 
-  // End initialization section
-
-  uint32_t address = 0;
-  uint16_t i = 0;
-
+  // Check for trigger
   bool in_bootloader = false;
-  uint16_t j = ENTER_WAIT;
+  uint16_t j = INITIAL_WAIT;
   while (!in_bootloader && 0 < j--) {
-    // Check for trigger
     if (uart_has_char()) in_bootloader = uart_recv_char() == CMD_SYNC;
-
+    watchdog_reset();
     _delay_ms(1);
   }
 
-  WDT_EnableAndSetTimeout();
-
   // Main bootloader
+  uint32_t address = 0;
+  uint16_t i = 0;
+
   while (in_bootloader) {
     uint8_t val = get_char();
-    WDT_Reset();
+    watchdog_reset();
 
     // Main bootloader parser
     switch (val) {
@@ -107,20 +251,23 @@ int main() {
       send_char(REPLY_ACK);
       break;
 
-    case CMD_SET_EXT_ADDRESS:
-      address = ((uint32_t)get_char() << 16) | get_word();
+    case CMD_SET_EXT_ADDRESS: {
+      uint8_t hi = get_char();
+      address = ((uint32_t)hi << 16) | get_word();
       send_char(REPLY_ACK);
       break;
+    }
 
     case CMD_CHIP_ERASE:
       // Erase the application section
       SP_EraseApplicationSection();
 
       // Wait for completion
-      NVM_Wait();
+      nvm_wait();
 
       // Erase EEPROM
-      EEPROM_erase_all();
+      NVM.CMD = NVM_CMD_ERASE_EEPROM_gc;
+      nvm_exec();
 
       send_char(REPLY_ACK);
       break;
@@ -128,8 +275,8 @@ int main() {
     case CMD_CHECK_BLOCK_SUPPORT:
       send_char(REPLY_YES);
       // Send block size (page size)
-      send_char((SPM_PAGESIZE >> 8) & 0xFF);
-      send_char(SPM_PAGESIZE & 0xFF);
+      send_char(SPM_PAGESIZE >> 8);
+      send_char((uint8_t)SPM_PAGESIZE);
       break;
 
     case CMD_BLOCK_LOAD:
@@ -145,7 +292,7 @@ int main() {
       break;
 
     case CMD_READ_BYTE: {
-      unsigned w = SP_ReadWord((address << 1));
+      unsigned w = SP_ReadWord(address << 1);
 
       send_char(w >> 8);
       send_char(w);
@@ -161,7 +308,7 @@ int main() {
 
     case CMD_WRITE_HIGH_BYTE:
       i |= (get_char() << 8); // get high byte; combine
-      SP_LoadFlashWord((address << 1), i);
+      SP_LoadFlashWord(address << 1, i);
       address++;
       send_char(REPLY_ACK);
       break;
@@ -177,13 +324,13 @@ int main() {
       break;
 
     case CMD_WRITE_EEPROM_BYTE:
-      EEPROM_write_byte(address, get_char());
+      eeprom_write_byte((uint8_t *)(uint16_t)address, get_char());
       address++;
       send_char(REPLY_ACK);
       break;
 
     case CMD_READ_EEPROM_BYTE:
-      send_char(EEPROM_read_byte(address));
+      send_char(eeprom_read_byte((uint8_t *)(uint16_t)address));
       address++;
       break;
 
@@ -213,18 +360,18 @@ int main() {
       break;
 
     case CMD_PROGRAM_ID:
-      send_char('X');
-      send_char('B');
-      send_char('o');
-      send_char('o');
+      send_char('b');
+      send_char('b');
+      send_char('c');
       send_char('t');
-      send_char('+');
-      send_char('+');
+      send_char('r');
+      send_char('l');
+      send_char(' ');
       break;
 
     case CMD_VERSION:
-      send_char('0' + XBOOT_VERSION_MAJOR);
-      send_char('0' + XBOOT_VERSION_MINOR);
+      send_char('0');
+      send_char('1');
       break;
 
     case CMD_READ_SIGNATURE:
@@ -233,32 +380,10 @@ int main() {
       send_char(SIGNATURE_0);
       break;
 
-    case CMD_CRC: {
-      uint32_t start = 0;
-      uint32_t length = 0;
-      uint16_t crc;
-
-      val = get_char();
-
-      switch (val) {
-      case SECTION_FLASH: length = PROGMEM_SIZE; break;
-      case SECTION_APPLICATION: length = APP_SECTION_SIZE; break;
-      case SECTION_BOOT:
-        start = BOOT_SECTION_START;
-        length = BOOT_SECTION_SIZE;
-        break;
-
-      default:
-        send_char(REPLY_ERROR);
-        continue;
-      }
-
-      crc = crc16_block(start, length);
-
-      send_char((crc >> 8) & 0xff);
-      send_char(crc & 0xff);
+    case CMD_BLOCK_CRC:
+      send_char(block_crc >> 8);
+      send_char((uint8_t)block_crc);
       break;
-    }
 
     case CMD_SYNC: break; // ESC (0x1b) to sync
 
@@ -268,134 +393,16 @@ int main() {
     }
 
     // Wait for any lingering SPM instructions to finish
-    NVM_Wait();
+    nvm_wait();
   }
 
-  // Bootloader exit
-  // Code here runs after the bootloader has exited,
-  // but before the application code has started
-
-  // Shut down UART
+  // Deinit
   uart_deinit();
+  watchdog_disable();
 
-  WDT_Disable();
+  // Disable further self programming until next reset
+  SP_LockSPM();
 
-  // Jump into main code
+  // Jump to application code
   asm("jmp 0");
-}
-
-
-uint8_t get_char() {
-  while (!uart_has_char()) continue;
-  return uart_recv_char();
-}
-
-
-void send_char(uint8_t c) {uart_send_char_blocking(c);}
-uint16_t get_word() {return (get_char() << 8) | get_char();}
-
-
-uint8_t BlockLoad(unsigned size, uint8_t mem, uint32_t *address) {
-  WDT_Reset();
-
-  // fill up buffer
-  for (int i = 0; i < SPM_PAGESIZE; i++) {
-    char c = 0xff;
-    if (i < size) c = get_char();
-    buffer[i] = c;
-  }
-
-  switch (mem) {
-  case MEM_EEPROM:
-    EEPROM_write_block(*address, buffer, size);
-    *address += size;
-    break;
-
-  case MEM_FLASH:
-    // NOTE: Flash programming, address is given in words.
-    SP_LoadFlashPage(buffer);
-    SP_EraseWriteApplicationPage(*address << 1);
-    *address += size >> 1;
-    NVM_Wait();
-    break;
-
-  case MEM_USERSIG:
-    SP_LoadFlashPage(buffer);
-    SP_EraseUserSignatureRow();
-    NVM_Wait();
-    SP_WriteUserSignatureRow();
-    NVM_Wait();
-    break;
-
-  default: return REPLY_ERROR;
-  }
-
-  return REPLY_ACK;
-}
-
-
-
-void BlockRead(unsigned size, uint8_t mem, uint32_t *address) {
-  int offset = 0;
-  int size2 = size;
-
-  switch (mem) {
-  case MEM_EEPROM:
-    EEPROM_read_block(*address, buffer, size);
-    (*address) += size;
-    break;
-
-  case MEM_FLASH: case MEM_USERSIG: case MEM_PRODSIG: {
-    *address <<= 1; // Convert address to bytes temporarily
-
-    do {
-      switch (mem) {
-      case MEM_FLASH: buffer[offset++] = SP_ReadByte(*address); break;
-
-      case MEM_USERSIG:
-        buffer[offset++] = SP_ReadUserSignatureByte(*address);
-        break;
-
-      case MEM_PRODSIG:
-        buffer[offset++] = SP_ReadCalibrationByte(*address);
-        break;
-      }
-
-      NVM_Wait();
-
-      (*address)++;    // Select next word in memory.
-      size--;          // Subtract two bytes from number of bytes to read
-    } while (size);    // Repeat until all block has been read
-
-    *address >>= 1;    // Convert address back to Flash words again.
-    break;
-  }
-
-  default: send_char(REPLY_ERROR); break;
-  }
-
-  // send bytes
-  for (int i = 0; i < size2; i++)
-    send_char(buffer[i]);
-}
-
-
-uint16_t crc16_block(uint32_t start, uint32_t length) {
-  uint16_t crc = 0;
-
-  int bc = SPM_PAGESIZE;
-
-  for (; length > 0; length--) {
-    if (bc == SPM_PAGESIZE) {
-      SP_ReadFlashPage(buffer, start);
-      start += SPM_PAGESIZE;
-      bc = 0;
-    }
-
-    crc = _crc16_update(crc, buffer[bc]);
-
-    bc++;
-  }
-
-  return crc;
 }
--- avr/src/xboot/xboot.h
+++ avr/src/xboot/xboot.h
@@ -1,168 +1,108 @@
-/************************************************************************/
-/* XBoot Extensible AVR Bootloader                                      */
-/*                                                                      */
-/* xboot.h                                                              */
-/*                                                                      */
-/* Alex Forencich <alex@alexforencich.com>                              */
-/*                                                                      */
-/* Copyright (c) 2010 Alex Forencich                                    */
-/*                                                                      */
-/* Permission is hereby granted, free of charge, to any person          */
-/* obtaining a copy of this software and associated documentation       */
-/* files(the "Software"), to deal in the Software without restriction,  */
-/* including without limitation the rights to use, copy, modify, merge, */
-/* publish, distribute, sublicense, and/or sell copies of the Software, */
-/* and to permit persons to whom the Software is furnished to do so,    */
-/* subject to the following conditions:                                 */
-/*                                                                      */
-/* The above copyright notice and this permission notice shall be       */
-/* included in all copies or substantial portions of the Software.      */
-/*                                                                      */
-/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,      */
-/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF   */
-/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND                */
-/* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS  */
-/* BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN   */
-/* ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN    */
-/* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE     */
-/* SOFTWARE.                                                            */
-/*                                                                      */
-/************************************************************************/
+/******************************************************************************\
+
+                This file is part of the Buildbotics firmware.
+
+                  Copyright (c) 2015 - 2016 Buildbotics LLC
+          Copyright (c) 2010 Alex Forencich <alex@alexforencich.com>
+                            All rights reserved.
+
+     This file ("the software") is free software: you can redistribute it
+     and/or modify it under the terms of the GNU General Public License,
+      version 2 as published by the Free Software Foundation. You should
+      have received a copy of the GNU General Public License, version 2
+     along with the software. If not, see <http://www.gnu.org/licenses/>.
+
+     The software is distributed in the hope that it will be useful, but
+          WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+               Lesser General Public License for more details.
+
+       You should have received a copy of the GNU Lesser General Public
+                License along with the software.  If not, see
+                       <http://www.gnu.org/licenses/>.
+
+                For information regarding this software email:
+                  "Joseph Coffland" <joseph@buildbotics.com>
+
+\******************************************************************************/
 
 #pragma once
 
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <util/crc16.h>
+#define INITIAL_WAIT 1000 // In ms
 
-// token pasting
-#define CAT2_int(x, y) x ## y
-#define CAT2(x, y) CAT2_int(x, y)
-#define CAT3_int(x, y, z) x ## y ## z
-#define CAT3(x, y, z) CAT3_int(x, y, z)
-
-// Version
-#define XBOOT_VERSION_MAJOR 1
-#define XBOOT_VERSION_MINOR 7
-
-// Configuration
-#define ENTER_WAIT 1000 // In ms
-#define UART_BAUD_RATE 115200
-#define UART_NUMBER 0
-#define UART_PORT_NAME C
-#define USE_AVR1008_EEPROM
-#define USE_DFLL
-#define WATCHDOG_TIMEOUT WDT_PER_1KCLK_gc
-
-// clock config
-// use 32MHz osc if makefile calls for it
-#if (F_CPU == 32000000L)
-// defaults to 2MHz RC oscillator
-// define USE_32MHZ_RC to override
-#define USE_32MHZ_RC
-#endif // F_CPU
-
-// UART RS485 Enable Output
-#define UART_EN_PORT            CAT2(PORT, UART_EN_PORT_NAME)
-
-#if (UART_NUMBER == 0)
 #define UART_RX_PIN             2
 #define UART_TX_PIN             3
-#else
-#define UART_RX_PIN             6
-#define UART_TX_PIN             7
-#endif
-#define UART_PORT               CAT2(PORT, UART_PORT_NAME)
-#define UART_DEVICE_PORT        CAT2(UART_PORT_NAME, UART_NUMBER)
-#define UART_DEVICE             CAT2(USART, UART_DEVICE_PORT)
-#define UART_DEVICE_RXC_ISR     CAT3(USART, UART_DEVICE_PORT, _RXC_vect)
-#define UART_DEVICE_DRE_ISR     CAT3(USART, UART_DEVICE_PORT, _DRE_vect)
-#define UART_DEVICE_TXC_ISR     CAT3(USART, UART_DEVICE_PORT, _TXC_vect)
-#define UART_RX_PIN_CTRL        CAT3(UART_PORT.PIN, UART_RX_PIN, CTRL)
-#define UART_TX_PIN_CTRL        CAT3(UART_PORT.PIN, UART_TX_PIN, CTRL)
+#define UART_PORT               PORTC
+#define UART_DEVICE             USARTC0
 
-// BAUD Rate Values
-// Known good at 2MHz
-#if (F_CPU == 2000000L) && (UART_BAUD_RATE == 19200)
-#define UART_BSEL_VALUE         12
-#define UART_BSCALE_VALUE       0
-#define UART_CLK2X              1
-#elif (F_CPU == 2000000L) && (UART_BAUD_RATE == 38400)
-#define UART_BSEL_VALUE         22
-#define UART_BSCALE_VALUE       -2
-#define UART_CLK2X              1
-#elif (F_CPU == 2000000L) && (UART_BAUD_RATE == 57600)
-#define UART_BSEL_VALUE         26
-#define UART_BSCALE_VALUE       -3
-#define UART_CLK2X              1
-#elif (F_CPU == 2000000L) && (UART_BAUD_RATE == 115200)
-#define UART_BSEL_VALUE         19
-#define UART_BSCALE_VALUE       -4
-#define UART_CLK2X              1
-
-// Known good at 32MHz
-#elif (F_CPU == 32000000L) && (UART_BAUD_RATE == 19200)
-#define UART_BSEL_VALUE         103
-#define UART_BSCALE_VALUE       0
-#define UART_CLK2X              0
-#elif (F_CPU == 32000000L) && (UART_BAUD_RATE == 38400)
-#define UART_BSEL_VALUE         51
-#define UART_BSCALE_VALUE       0
-#define UART_CLK2X              0
-#elif (F_CPU == 32000000L) && (UART_BAUD_RATE == 57600)
-#define UART_BSEL_VALUE         34
-#define UART_BSCALE_VALUE       0
-#define UART_CLK2X              0
-#elif (F_CPU == 32000000L) && (UART_BAUD_RATE == 115200)
+// Baud rate 115200 @ 32Mhz
 #define UART_BSEL_VALUE         1047
 #define UART_BSCALE_VALUE       -6
-#define UART_CLK2X              0
 
-#else // None of the above, so calculate something
-#warning Not using predefined BAUD rate, possible BAUD rate error!
-#if (F_CPU == 2000000L)
-#define UART_BSEL_VALUE         ((F_CPU) / ((uint32_t)UART_BAUD_RATE * 8) - 1)
-#define UART_BSCALE_VALUE       0
-#define UART_CLK2X              1
 
-#else
-#define UART_BSEL_VALUE         ((F_CPU) / ((uint32_t)UART_BAUD_RATE * 16) - 1)
-#define UART_BSCALE_VALUE       0
-#define UART_CLK2X              0
-#endif
-#endif
+// Protocol
+enum {
+  CMD_SYNC                = '\x1b',
 
-#ifndef EEPROM_PAGE_SIZE
-#define EEPROM_PAGE_SIZE E2PAGESIZE
-#endif
+  // Informational
+  CMD_CHECK_AUTOINCREMENT = 'a',
+  CMD_CHECK_BLOCK_SUPPORT = 'b',
+  CMD_PROGRAMMER_TYPE     = 'p',
+  CMD_DEVICE_CODE         = 't',
+  CMD_PROGRAM_ID          = 'S',
+  CMD_VERSION             = 'V',
+  CMD_HW_VERSION          = 'v', // Unsupported extension
+  CMD_READ_SIGNATURE      = 's',
 
-#ifndef EEPROM_BYTE_ADDRESS_MASK
-#if EEPROM_PAGE_SIZE == 32
-#define EEPROM_BYTE_ADDRESS_MASK 0x1f
-#elif EEPROM_PAGE_SIZE == 16
-#define EEPROM_BYTE_ADDRESS_MASK = 0x0f
-#elif EEPROM_PAGE_SIZE == 8
-#define EEPROM_BYTE_ADDRESS_MASK = 0x07
-#elif EEPROM_PAGE_SIZE == 4
-#define EEPROM_BYTE_ADDRESS_MASK = 0x03
-#else
-#error Unknown EEPROM page size!  Please add new byte address value!
-#endif
-#endif
+  // Addressing
+  CMD_SET_ADDRESS         = 'A',
+  CMD_SET_EXT_ADDRESS     = 'H',
 
-// Includes
-#include "protocol.h"
-#include "sp_driver.h"
-#include "eeprom_driver.h"
-#include "uart.h"
-#include "watchdog.h"
+  // Erase
+  CMD_CHIP_ERASE          = 'e',
 
-// Functions
-uint8_t get_char(void);
-void send_char(uint8_t c);
-uint16_t get_word(void);
+  // Block Access
+  CMD_BLOCK_LOAD          = 'B',
+  CMD_BLOCK_READ          = 'g',
+  CMD_BLOCK_CRC           = 'i',
 
-uint8_t BlockLoad(unsigned size, uint8_t mem, uint32_t *address);
-void BlockRead(unsigned size, uint8_t mem, uint32_t *address);
+  // Byte Access
+  CMD_READ_BYTE           = 'R',
+  CMD_WRITE_LOW_BYTE      = 'c',
+  CMD_WRITE_HIGH_BYTE     = 'C',
+  CMD_WRITE_PAGE          = 'm',
+  CMD_WRITE_EEPROM_BYTE   = 'D',
+  CMD_READ_EEPROM_BYTE    = 'd',
 
-uint16_t crc16_block(uint32_t start, uint32_t length);
+  // Lock and Fuse Bits
+  CMD_WRITE_LOCK_BITS     = 'l',
+  CMD_READ_LOCK_BITS      = 'r',
+  CMD_READ_LOW_FUSE_BITS  = 'F',
+  CMD_READ_HIGH_FUSE_BITS = 'N',
+  CMD_READ_EXT_FUSE_BITS  = 'Q',
+
+  // Control
+  CMD_ENTER_PROG_MODE     = 'P',
+  CMD_LEAVE_PROG_MODE     = 'L',
+  CMD_EXIT_BOOTLOADER     = 'E',
+  CMD_SET_LED             = 'x',
+  CMD_CLEAR_LED           = 'y',
+  CMD_SET_TYPE            = 'T',
+};
+
+
+// Memory types for block access
+enum {
+  MEM_EEPROM              = 'E',
+  MEM_FLASH               = 'F',
+  MEM_USERSIG             = 'U',
+  MEM_PRODSIG             = 'P',
+};
+
+
+// Command Responses
+enum {
+  REPLY_ACK               = '\r',
+  REPLY_YES               = 'Y',
+  REPLY_ERROR             = '?',
+};
