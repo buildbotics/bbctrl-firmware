93a033b316022afc0b5727f7494d5c86edd42911
Joseph Coffland <joseph@cauldrondevelopment.com>
2018-02-14 22:49:09 -0800

    Added license tags

--- src/pwr/pins.c
+++ src/pwr/pins.c
@@ -1,27 +1,27 @@
 /******************************************************************************\
 
-                This file is part of the Buildbotics firmware.
+                 This file is part of the Buildbotics firmware.
 
-                  Copyright (c) 2015 - 2017 Buildbotics LLC
-                            All rights reserved.
+                   Copyright (c) 2015 - 2018, Buildbotics LLC
+                              All rights reserved.
 
-     This file ("the software") is free software: you can redistribute it
-     and/or modify it under the terms of the GNU General Public License,
-      version 2 as published by the Free Software Foundation. You should
-      have received a copy of the GNU General Public License, version 2
-     along with the software. If not, see <http://www.gnu.org/licenses/>.
+      This file ("the software") is free software: you can redistribute it
+      and/or modify it under the terms of the GNU General Public License,
+       version 2 as published by the Free Software Foundation. You should
+       have received a copy of the GNU General Public License, version 2
+      along with the software. If not, see <http://www.gnu.org/licenses/>.
 
-     The software is distributed in the hope that it will be useful, but
-          WITHOUT ANY WARRANTY; without even the implied warranty of
-      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-               Lesser General Public License for more details.
+      The software is distributed in the hope that it will be useful, but
+           WITHOUT ANY WARRANTY; without even the implied warranty of
+       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+                Lesser General Public License for more details.
 
-       You should have received a copy of the GNU Lesser General Public
-                License along with the software.  If not, see
-                       <http://www.gnu.org/licenses/>.
+        You should have received a copy of the GNU Lesser General Public
+                 License along with the software.  If not, see
+                        <http://www.gnu.org/licenses/>.
 
-                For information regarding this software email:
-                  "Joseph Coffland" <joseph@buildbotics.com>
+                 For information regarding this software email:
+                   "Joseph Coffland" <joseph@buildbotics.com>
 
 \******************************************************************************/
 
