56d592958ae9e925b53c4134e7c25d3de2a4c0bf
Joseph Coffland <joseph@cauldrondevelopment.com>
2018-11-14 03:18:44 -0800

    Fix for gplan build env setup

--- scripts/gplan-init-dev-img.sh
+++ scripts/gplan-init-dev-img.sh
@@ -5,7 +5,7 @@ cd /mnt/host
 
 # Update the system
 apt-get update
-apt-get dist-upgrade -y
+#apt-get dist-upgrade -y
 
 # Install packages
 apt-get install -y scons build-essential libssl-dev python3-dev
