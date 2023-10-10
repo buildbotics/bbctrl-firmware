11d9baba6307c573265a46286f74799587883800
Joseph Coffland <joseph@cauldrondevelopment.com>
2023-10-10 15:27:54 +0300

    Fix partition resizing

--- src/overlay/root/root/resize.sh
+++ src/overlay/root/root/resize.sh
@@ -3,15 +3,16 @@
 DISK=mmcblk0
 PATH=$PATH:/usr/sbin
 
+mount -t proc proc /proc
+mount -t sysfs sys /sys
+mount -o remount,rw /
+
 if [ -e /root/resize2 ]; then
   mount /dev/${DISK}p1 /boot
   sed -i 's|init=/root/resize.sh||' /boot/cmdline.txt
   umount /boot
 
-  mount -t proc proc /proc
-  mount -t sysfs sys /sys
-  mount -o remount,rw /
-
+  echo Resizing filesystem
   resize2fs -f /dev/${DISK}p2
 
   rm /root/resize.sh /root/resize2
@@ -19,12 +20,16 @@ if [ -e /root/resize2 ]; then
   exec /sbin/init "$@"
 
 else
-  mount -o remount,rw /
   touch /root/resize2
 
+  echo Resizing partition
   printf "w\ny\ny\n" | gdisk /dev/$DISK
   parted -s /dev/$DISK -- unit s resizepart 2 -34s
 fi
 
 mount -o remount,ro /
+
+# Force a reboot
+echo 1 > /proc/sys/kernel/sysrq
+echo b > /proc/sysrq-trigger
 reboot -f
